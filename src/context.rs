6cba2bd970826a4383089b49dc313055f7a477df
Sebastiano Faiella <xeno097.cp@gmail.com>
2023-03-24 22:29:23 -0400

    feat: updated context to hold an instance of an ethers provider

--- src/context.rs
+++ src/context.rs
@@ -1,17 +1,22 @@
 use crate::config::CliConfig;
+use ethers::providers::{Http, Provider};
 use std::future::Future;
 use tokio::runtime;
 
 pub struct CommandExecutionContext {
     config: CliConfig,
     runtime: runtime::Runtime,
+    node_provider: Provider<Http>,
 }
 
 impl CommandExecutionContext {
     pub fn new(config: CliConfig) -> Self {
+        let node_provider = ethers::providers::Provider::try_from(config.rpc_url()).unwrap();
+
         Self {
             config,
             runtime: runtime::Runtime::new().unwrap(),
+            node_provider,
         }
     }
 
@@ -24,5 +29,9 @@ impl CommandExecutionContext {
 
     pub fn config(&self) -> &CliConfig {
         &self.config
+    }
+
+    pub fn node_provider(&self) -> &Provider<Http> {
+        &self.node_provider
     }
 }
