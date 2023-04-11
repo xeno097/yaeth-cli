a3306d1b86abb3ee60c4d360ab407e44200de338
Sebastiano Faiella <xeno097.cp@gmail.com>
2023-04-10 20:15:32 -0400

    refactor(account): replaced execution context with node provider in account methods

--- src/cmd/account.rs
+++ src/cmd/account.rs
@@ -3,44 +3,37 @@ use ethers::{
     types::{BlockId, BlockNumber, Bytes, NameOrAddress, H256, U256},
 };
 
-use crate::context::CommandExecutionContext;
+use crate::context::NodeProvider;
 
 // eth_getBalance
 pub async fn get_balance(
-    context: &CommandExecutionContext,
+    node_provider: &NodeProvider,
     account_id: NameOrAddress,
     block_id: Option<BlockId>,
 ) -> anyhow::Result<U256> {
-    let balance = context
-        .node_provider()
-        .get_balance(account_id, block_id)
-        .await?;
+    let balance = node_provider.get_balance(account_id, block_id).await?;
 
     Ok(balance)
 }
 
 // eth_getCode
 pub async fn get_code(
-    context: &CommandExecutionContext,
+    node_provider: &NodeProvider,
     account_id: NameOrAddress,
     block_id: Option<BlockId>,
 ) -> anyhow::Result<Bytes> {
-    let bytecode = context
-        .node_provider()
-        .get_code(account_id, block_id)
-        .await?;
+    let bytecode = node_provider.get_code(account_id, block_id).await?;
 
     Ok(bytecode)
 }
 
 // eth_getTransactionCount
 pub async fn get_transaction_count(
-    context: &CommandExecutionContext,
+    node_provider: &NodeProvider,
     account_id: NameOrAddress,
     block_id: Option<BlockId>,
 ) -> anyhow::Result<U256> {
-    let transaction_count = context
-        .node_provider()
+    let transaction_count = node_provider
         .get_transaction_count(account_id, block_id)
         .await?;
 
@@ -48,11 +41,11 @@ pub async fn get_transaction_count(
 }
 
 pub async fn get_nonce(
-    context: &CommandExecutionContext,
+    node_provider: &NodeProvider,
     account_id: NameOrAddress,
 ) -> anyhow::Result<U256> {
     get_transaction_count(
-        context,
+        node_provider,
         account_id,
         Some(BlockId::Number(BlockNumber::Pending)),
     )
@@ -62,13 +55,12 @@ pub async fn get_nonce(
 // eth_getStorageAt
 // TODO: Implement a variant that recieves the expected storage slot type and parses the result based on that
 pub async fn get_storage_at(
-    context: &CommandExecutionContext,
+    node_provider: &NodeProvider,
     account_id: NameOrAddress,
     slot: H256,
     block_id: Option<BlockId>,
 ) -> anyhow::Result<H256> {
-    let storage_data = context
-        .node_provider()
+    let storage_data = node_provider
         .get_storage_at(account_id, slot, block_id)
         .await?;
 
@@ -81,12 +73,12 @@ mod tests {
     mod get_balance {
         use ethers::utils::parse_ether;
 
-        use crate::cmd::{account::get_balance, helpers::test::setup_test};
+        use crate::cmd::{account::get_balance, helpers::test::setup_test_with_no_context};
 
-        #[test]
-        fn should_get_the_account_balance() -> anyhow::Result<()> {
+        #[tokio::test]
+        async fn should_get_the_account_balance() -> anyhow::Result<()> {
             // Arrange
-            let (execution_context, anvil) = setup_test()?;
+            let (node_provider, anvil) = setup_test_with_no_context().await?;
 
             let account = *anvil.addresses().get(0).unwrap();
 
@@ -94,8 +86,7 @@ mod tests {
             let expected_balance = parse_ether(10_000)?;
 
             // Act
-            let res =
-                execution_context.execute(get_balance(&execution_context, account.into(), None));
+            let res = get_balance(&node_provider, account.into(), None).await;
 
             // Assert
             assert!(res.is_ok());
@@ -108,17 +99,17 @@ mod tests {
     }
 
     mod get_code {
-        use crate::cmd::{account::get_code, helpers::test::setup_test};
+        use crate::cmd::{account::get_code, helpers::test::setup_test_with_no_context};
 
-        #[test]
-        fn should_get_the_account_code() -> anyhow::Result<()> {
+        #[tokio::test]
+        async fn should_get_the_account_code() -> anyhow::Result<()> {
             // Arrange
-            let (execution_context, anvil) = setup_test()?;
+            let (node_provider, anvil) = setup_test_with_no_context().await?;
 
             let account = *anvil.addresses().get(0).unwrap();
 
             // Act
-            let res = execution_context.execute(get_code(&execution_context, account.into(), None));
+            let res = get_code(&node_provider, account.into(), None).await;
 
             // Assert
             assert!(res.is_ok());
@@ -133,21 +124,19 @@ mod tests {
     mod get_transaction_count {
         use ethers::types::U256;
 
-        use crate::cmd::{account::get_transaction_count, helpers::test::setup_test};
+        use crate::cmd::{
+            account::get_transaction_count, helpers::test::setup_test_with_no_context,
+        };
 
-        #[test]
-        fn should_get_the_account_transaction_count() -> anyhow::Result<()> {
+        #[tokio::test]
+        async fn should_get_the_account_transaction_count() -> anyhow::Result<()> {
             // Arrange
-            let (execution_context, anvil) = setup_test()?;
+            let (node_provider, anvil) = setup_test_with_no_context().await?;
 
             let account = *anvil.addresses().get(0).unwrap();
 
             // Act
-            let res = execution_context.execute(get_transaction_count(
-                &execution_context,
-                account.into(),
-                None,
-            ));
+            let res = get_transaction_count(&node_provider, account.into(), None).await;
 
             // Assert
             assert!(res.is_ok());
@@ -164,22 +153,17 @@ mod tests {
     mod get_storage_at {
         use ethers::types::H256;
 
-        use crate::cmd::{account::get_storage_at, helpers::test::setup_test};
+        use crate::cmd::{account::get_storage_at, helpers::test::setup_test_with_no_context};
 
-        #[test]
-        fn should_get_the_storage_data_in_the_selected_slot() -> anyhow::Result<()> {
+        #[tokio::test]
+        async fn should_get_the_storage_data_in_the_selected_slot() -> anyhow::Result<()> {
             // Arrange
-            let (execution_context, anvil) = setup_test()?;
+            let (node_provider, anvil) = setup_test_with_no_context().await?;
 
             let account = *anvil.addresses().get(0).unwrap();
 
             // Act
-            let res = execution_context.execute(get_storage_at(
-                &execution_context,
-                account.into(),
-                H256::default(),
-                None,
-            ));
+            let res = get_storage_at(&node_provider, account.into(), H256::default(), None).await;
 
             // Assert
             assert!(res.is_ok());
